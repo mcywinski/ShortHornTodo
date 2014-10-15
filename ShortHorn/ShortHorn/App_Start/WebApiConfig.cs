9b7e55e86226a36bcb51662fdd48a5e5f5248981
Michał Cywiński <mcywinski@hotmail.com>
2014-10-15 23:12:35 +0200

    Updated Web API configuration

--- ShortHorn/ShortHorn/App_Start/WebApiConfig.cs
+++ ShortHorn/ShortHorn/App_Start/WebApiConfig.cs
@@ -9,11 +9,10 @@ namespace ShortHorn
     {
         public static void Register(HttpConfiguration config)
         {
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.Routes.MapHttpRoute("DefaultApiWithId", "Api/{controller}/{id}", new { id = RouteParameter.Optional }, new { id = @"\d+" });
+            config.Routes.MapHttpRoute("DefaultApiWithAction", "Api/{controller}/{action}");
+            config.Routes.MapHttpRoute("DefaultApiGet", "Api/{controller}", new { action = "Get" }, new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) });
+            config.Routes.MapHttpRoute("DefaultApiPost", "Api/{controller}", new { action = "Post" }, new { httpMethod = new HttpMethodConstraint(HttpMethod.Post) });
         }
     }
 }
