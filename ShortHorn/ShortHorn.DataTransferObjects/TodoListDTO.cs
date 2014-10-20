a3bae121d31d3e6d8e6cc2bfc2d50d97cbf7c544
Michał Cywiński <mcywinski@hotmail.com>
2014-10-20 22:16:41 +0200

    Added todolist DTO.

---
+++ ShortHorn/ShortHorn.DataTransferObjects/TodoListDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortHorn.DataTransferObjects
+{
+    /// <summary>
+    /// DTO representation of todo list.
+    /// </summary>
+    public class TodoListDTO : BaseDTO
+    {
+        /// <summary>
+        /// Name of the list.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of the list.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Indication of favourite status of the list.
+        /// </summary>
+        public bool IsFavourite { get; set; }
+    }
+}
