8988619f1c141ab388b4c2550739e3bc0176d080
Michał Cywiński <mcywinski@hotmail.com>
2014-10-22 23:40:38 +0200

    Added Todo Item DTO.

---
+++ ShortHorn/ShortHorn.DataTransferObjects/TodoItemDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortHorn.DataTransferObjects
+{
+    /// <summary>
+    /// DTO representation of Todo Item.
+    /// </summary>
+    public class TodoItemDTO : BaseDTO
+    {
+        /// <summary>
+        /// The name of Todo Item.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Detailed information about Todo item.
+        /// </summary>
+        public string Details { get; set; }
+
+        /// <summary>
+        /// Finish indicator.
+        /// </summary>
+        public bool IsFinished { get; set; }
+
+        /// <summary>
+        /// Favourite indicator.
+        /// </summary>
+        public bool IsFavourite { get; set; }
+    }
+}
