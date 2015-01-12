6a8ab458af20e7f9d232c69241571b36b7e513dc
Michał Cywiński <mcywinski@hotmail.com>
2015-01-12 09:41:58 +0100

    Added WP 8.0 compatible app.

---
+++ ShortHorn/ShortHorn.WPApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ShortHorn.WPApp.Resources;
+
+namespace ShortHorn.WPApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
