1cd594856b67cb2ddc208f2a4ab442bd16913ff7
Michał Cywiński <mcywinski@hotmail.com>
2015-01-12 10:37:10 +0100

    Added base desktop application with service skeleton.

---
+++ ShortHorn/ShortHornDesktop/Services/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortHornDesktop.Services
+{
+    public class BaseService
+    {
+        protected readonly string apiBaseUrl;
+
+        public BaseService(string apiBaseUrl)
+        {
+            this.apiBaseUrl = apiBaseUrl;
+        }
+    }
+}
---
+++ ShortHorn/ShortHornDesktop/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using ShortHorn.DataTransferObjects;
+
+namespace ShortHornDesktop.Services
+{
+    class UserService : BaseService
+    {
+        
+
+        public UserService(string apiBaseUrl)
+            : base(apiBaseUrl)
+        {
+
+        }
+
+        public async Task<string> Login(string login, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.apiBaseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/users", new LoginCredentialsDTO()
+                {
+                    Login = login,
+                    Password = password
+                });
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
